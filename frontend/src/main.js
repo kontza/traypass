581395be9852c695c96f3995cdce28b3bb9085b6
Juha Ruotsalainen <juha.ruotsalainen@iki.fi>
2024-09-27 06:43:55 +0300

    calcSize is not needed anymore, since we're not using a <select>

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,18 +1,5 @@
-const calcSize = () => {
-  const input = htmx.findAll('input')[0]
-  const inputHeight = input.getBoundingClientRect().height
-  const option = htmx.findAll('option')[0]
-  const optionHeight = option.getBoundingClientRect().height
-  const listSize = (window.innerHeight - inputHeight) / optionHeight
-  htmx.findAll('select')[0].size = listSize
-}
-window.addEventListener('pageshow', calcSize)
-window.addEventListener('resize', calcSize)
-
-const ESC = 'Escape'
-
 document.addEventListener('keyup', (e) => {
-  if (e.key === ESC) {
+  if (e.key === 'Escape') {
     const input = document.getElementsByName('filter').item(0)
     if (document.activeElement.nodeName === 'INPUT') {
       input.value = ''
